f2cd2017bf3a30e404c39f37f05ce11c500cbd1b
AaronGoh <aaronwgoh5@gmail.com>
2019-04-01 10:38:14 -0600

    Image Replication successful, CSS somewhat messy but currently functional

--- SorensonDashboardDevelopment/imageReplication.R
+++ SorensonDashboardDevelopment/imageReplication.R
@@ -3,6 +3,9 @@ library(shinydashboard)
 library(shinythemes)
 library(tidyverse)
 
+
+
+
 #Map box outputs for generic purposes
 value <- map(1:6, function(.x) {
   uiOutput(paste0(.x))
@@ -15,6 +18,9 @@ body <- dashboardBody(
     tags$head(
       tags$link(rel = "stylesheet", type = "text/css", href = "css/main.css")
     ),
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "css/bootstrap.css")
+    ),
             
     #Define the dropdown menu 
     selectInput("boxId", "Fill a Box:",
@@ -24,53 +30,79 @@ body <- dashboardBody(
     br(), #Space out between the dropdown
     
     #Creates two rows with 2 boxes per row
-    tags$div(class="container-fluid",
+    tags$div(class="container", id="imageInfo",
       fluidRow(
-        tags$div(id="standard-box", class = "col-sm-3 row1 label1"),
-        tags$div(id="standard-box", class = "col-sm-3 row1 label2",
-                 tags$p("Healthy Beginnings")
+        tags$div(id="standard-box", class = "col-3 row1 col1 box1"),
+        tags$div(id="standard-box", class = "col-3 row1 col2 label1 box2",
+           tags$div(class = "content", 
+              tags$p("Healthy Beginnings")
+           )
         ),
-        tags$div(id="standard-box", class = "col-sm-3 row1 label3",
-                 tags$p("Supported Families")
+        tags$div(id="standard-box", class = "col-3 row1 col3 label2 box3 ",
+           tags$div(class = "content", 
+              tags$p("Supported Families")
+           )
         ),
-        tags$div(id="standard-box", class = "col-sm-3 row1 label4",
-                 tags$p("Quality Care and Learning")
+        tags$div(id="standard-box", class = "col-3 row1 col4 label3 box4",
+           tags$div(class = "content", 
+              tags$p("Quality Care and Learning")
+           )
         )
       ),
       fluidRow(
-        tags$div(id="standard-box", class = "col-sm-3 row1 label1",
-                 tags$p("Child Family and Outcomes")
+        tags$div(id="standard-box", class = "col-3 row2 col1 label1 box5",
+           tags$div(class = "content", 
+              tags$p("Child Family and Outcomes")
+           )
         ),
-        tags$div(id="standard-box", class = "col-sm-3 row1 label2",
-                 uiOutput("box1")
+        tags$div(id="standard-box", class = "col-3 row2 col2 box6",
+           tags$div(class = "content icon-content", 
+              uiOutput("box1")
+           )
         ),
-        tags$div(id="standard-box", class = "col-sm-3 row1 label3",
-                 uiOutput("box2")
+        tags$div(id="standard-box", class = "col-3 row2 col3 box7",
+           tags$div(class = "content icon-content", 
+              uiOutput("box2")
+           )
         ),
-        tags$div(id="standard-box", class = "col-sm-3 row1 label4",
-                 uiOutput("box3")
+        tags$div(id="standard-box", class = "col-3 row2 col4 box8",
+           tags$div(class = "content icon-content", 
+              uiOutput("box3")
+           )
         )
       ),
       fluidRow(
-        tags$div(id="standard-box", class = "col-sm-3 row3 label1",
-                 tags$p("Prenatal to Three Program and Policy Expansion")
+        tags$div(id="standard-box", class = "col-3 row3 col1 label1 box9",
+           tags$div(class = "content", 
+              tags$p("Prenatal to Three Program and Policy Expansion")
+           )
         ),
-        tags$div(id="standard-box", class = "col-sm-3 row3 label2",
-                 uiOutput("box4")
+        tags$div(id="standard-box", class = "col-3 row3 col2 box10",
+           tags$div(class = "content icon-content", 
+              uiOutput("box4")
+           )
         ),
-        tags$div(id="standard-box", class = "col-sm-3 row3 label3",
-                 uiOutput("box5"), style= "border-"
+        tags$div(id="standard-box", class = "col-3 row3 col3 box11",
+           tags$div(class = "content icon-content", 
+              uiOutput("box5")
+           )
         ),
-        tags$div(id="standard-box", class = "col-sm-3 row3 label4",
-                 uiOutput("box6")
+        tags$div(id="standard-box", class = "col-3 row3 col4 box12",
+           tags$div(class = "content icon-content", 
+              uiOutput("box6")
+           )
         )
       ),
       fluidRow(
-        tags$div(id="standard-box", class = "col-sm-3 row4 label1",
-                 tags$p("Prenatal to Three Systems")
+        tags$div(id="standard-box", class = "col-3 row4 col1 label1 box13",
+           tags$div(class = "content", 
+              tags$p("Prenatal to Three Systems")
+           )
         ),
-        tags$div(id="standard-box", class = "row col-sm-9 row4 label2",
-                 uiOutput("box7")
+        tags$div(id="long-box", class = "col-9 row4 col2 box14",
+           tags$div(class = "content icon-content", 
+              uiOutput("box7")
+           )
         )
       )
     )
@@ -94,14 +126,12 @@ server <- function(input, output) {
     output[[paste0("box", .x)]] <- renderUI({ # paste0 concatenates vectors after converting to character.
       #Case where there should be a checkmark
       if (input$boxId == paste0(.x)) {
-        icon("ok", lib = "glyphicon")
+        icon("remove", lib = "glyphicon")
       }
     })
   })
 }
 
 #####################
-
 # Run the application 
 shinyApp(ui = ui, server = server)
-
